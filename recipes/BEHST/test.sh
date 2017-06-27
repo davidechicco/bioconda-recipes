de2a956a3e3ba7cb43b1b0e43f61667de6af5b89
Davide Chicco <davide.chicco@gmail.com>
2017-06-27 11:59:11 -0400

    Update test.sh

--- recipes/BEHST/test.sh
+++ recipes/BEHST/test.sh
@@ -5,10 +5,6 @@
 set -o nounset -o pipefail -o errexit
 set -o xtrace
 
-#mkdir -p $PREFIX/bin
-#cp $SRC_DIR/bin/* $PREFIX/bin
 cd $PREFIX/bin
 
-./project.sh ../data/pressto_BLOOD_enhancers.bed DEFAULT_EQ DEFAULT_ET
-
-./project.sh ../data/vista_LIMB_sorted.bed DEFAULT_EQ DEFAULT_ET
+./project.sh --test
