5466e4ad2cff495db4af51e5af856993f90c4e0f
davidechicco <davide.chicco@gmail.com>
2017-07-24 08:27:03 -0400

    new version with changes to dowload_data script

--- recipes/BEHST/build.sh
+++ recipes/BEHST/build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#$ -cwd
-#$ -S /bin/bash
-#
-set -o nounset -o pipefail -o errexit
-set -o xtrace
-
 mkdir -p $PREFIX/bin
-cp $SRC_DIR/bin/* $PREFIX/bin
-cd $PREFIX/bin
-
-./download_behst_data.sh ~/thisBEHSTdataFolder
+chmod +x bin/*
+cp bin/* $PREFIX/bin
